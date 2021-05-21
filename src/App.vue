855326943cf097af2b44c86aa428cd9ce4edbaa6
izumi-shibano <shibano.izumi@ynet-inc.com>
2021-05-21 09:31:28 +0900

    ヘッダー・フッターメニュー実装

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,21 @@
 <template>
   <v-app>
-    <header>
-      <v-app-bar
-        app
-        dark
-      >
-        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>MyPortfolioSite</v-toolbar-title>
-      </v-app-bar>
-      <v-navigation-drawer
-        v-model="drawer"
-        fixed
-        temporary
-      >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group>
-            <v-list-item>
-              <v-list-item-title>HOME</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>ABOUT</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>WORK</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>SERVICE</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>BLOG</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>ACCESS</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-    </header>
+    <AppHeader/>
+    <!-- ここから追加 -->
+    <AppFooter/>
+    <!-- ここまで追加 -->
   </v-app>
 </template>
  
 <script>
-import constants from './common/constants' // 今回追加
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue' // 今回追加
  
 export default {
-  data () {
-    return {
-      drawer: false,
-      menuItems: constants.menuItems // 今回追加
-    }
+  components: {
+    AppHeader,
+    AppFooter // 今回追加
   }
 }
 </script>
  
-<style></style>
