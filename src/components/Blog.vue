46ba07ef447a672b5492383caef0c0429b7031a7
izumi-shibano <shibano.izumi@ynet-inc.com>
2021-05-31 17:57:58 +0900

    ボタン等・テキスト等・テーブル等実装_20210531

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -32,10 +32,73 @@
       <input type="text" v-model="text" value="text">
     </div>
 
+  <!-- 'app'のタグはVue.jsによって操作されます。 -->
+  <div id="app">
+    <v-app>
+      <v-main>
+        <v-container>
+          <!-- ボタンを表示します。 -->
+          <v-btn>デフォルト</v-btn>
+          <v-btn color="primary">プライマリ</v-btn>
+          <v-btn color="secondary">セカンダリ</v-btn>
+          <v-btn color="error">エラー</v-btn>   
+          <v-btn rounded>デフォルト</v-btn>
+          <v-btn rounded color="primary">プライマリ</v-btn>
+          <v-btn rounded color="secondary">セカンダリ</v-btn>
+          <v-btn rounded color="error">エラー</v-btn>       
+          <v-checkbox v-model="isCheck" :label="`value: ${isCheck}`"></v-checkbox>
+          <!-- ラジオボタンを表示します。 -->
+          <v-radio-group v-model="radioGroup">
+            <v-radio label="Apple" :value="0"></v-radio>
+            <v-radio label="Banana" :value="1"></v-radio>
+            <v-radio label="Orange" :value="2"></v-radio>
+          </v-radio-group>          
+          <v-text-field label="Name"></v-text-field>
+          <v-text-field label="Password" type="password"></v-text-field>
+          <v-row>
+            <v-col cols="2">
+              <!-- リストボックスを表示します。 -->
+              <v-select
+                :items="items"
+                label="フルーツ">
+              </v-select>
+            </v-col>
+          </v-row>  
+          <v-row>
+            <v-col cols="2">
+              <!-- コンボボックスを表示します。 -->
+              <v-combobox
+                v-model="select"
+                :items="items2"
+                label="フルーツ">
+              </v-combobox>
+            </v-col>
+          </v-row> 
+          <v-row>
+            <v-col cols="2">
+              <!--
+                テキストエリアを表示します。 
+                outlined:枠線表示します。
+                clearable:クリア機能を追加します。
+              -->
+              <v-textarea
+                v-model="model"
+                outlined
+                clearable="true">
+              </v-textarea>
+            </v-col>
+          </v-row>                           
+          <Tableetc/>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+
   </section>
 </template>
 <script>
 import AppBackgroundHolder from './AppBackgroundHolder.vue'
+import Tableetc from './Tableetc.vue'
  
 export default {
   data () {
@@ -45,10 +108,17 @@ export default {
       checkedNames: [],
       radio: '',
       text: '',
-    }
+      isCheck: true,
+      radioGroup: 1,   
+      items: ['', 'Apple', 'Banana', 'Orange'],
+      select: ['未選択'],
+      items2: ['リンゴ', 'バナナ', 'オレンジ'],
+      model: '',
+       }
   },
   components: {
-    AppBackgroundHolder
+    AppBackgroundHolder,
+    Tableetc
   }
 }
 </script>
---
+++ src/components/Tableetc.vue
@@ -0,0 +1,93 @@
+<template>
+  <section>
+  <div id="app">
+    <v-app>
+     <v-container>
+        <v-data-table
+            :headers="headers"
+            :items="items">
+        </v-data-table>
+        <!-- グリッドを表示します。 -->
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">1-1</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">1-2</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">1-3</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">2-1</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">2-2</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-text class="text-center">2-3</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+     </v-container>
+   </v-app>
+  </div>
+  </section>
+</template>
+<script> 
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'No',
+          value: 'no',
+        },
+        {
+          text: '名前',
+          value: 'name',
+        },
+        {
+          text: '価格',
+          value: 'price',
+        },
+      ],
+      // テーブルのデータ行を定義します。
+      items: [
+        {
+          no: 1,
+          name: "リンゴ",
+          price: 100,
+        },
+        {
+          no: 2,
+          name: "バナナ",
+          price: 110,
+        },
+        {
+          no: 3,
+          name: "オレンジ",
+          price: 120,
+        },
+      ]
+    }
+  },
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
