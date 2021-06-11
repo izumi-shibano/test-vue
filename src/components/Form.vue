36685054a0721dfef9af39d777f36031bd073083
izumi-shibano <shibano.izumi@ynet-inc.com>
2021-06-11 19:09:18 +0900

    一応送信成功_20210611

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,14 @@
 <template>
   <section class="form">
+    <AppBackgroundHolder :title="title_f"/>
   <!-- 'app'のタグはVue.jsによって操作されます。 -->
    <div id="app">
     <v-app>
     <v-card>
       <v-card-title>STEP:0 フォーム</v-card-title>
-      <v-card-text>
-            <v-text-field
+        <v-form ref="formref">
+          <v-card-text>
+           <v-text-field
               v-model="title"
               :rules="titleRules"
               label="タイトル"
@@ -30,7 +32,6 @@
               required
             >
             </v-text-field>
-
             <v-text-field
               v-model="content"
               :rules="contentRules"
@@ -39,7 +40,8 @@
               required
             >
             </v-text-field>
-      </v-card-text>
+          </v-card-text>
+        </v-form>
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn text @click="submit">送信する</v-btn>
@@ -53,47 +55,61 @@
 </template>
 <script>
 import AppBackgroundHolder from './AppBackgroundHolder.vue'
+import axios from 'axios'
  
 export default {
+  components: {
+    AppBackgroundHolder,
+  },
+
   data () {
     return {
-        success: false,
-        title: '',
-        titleRules: [
-          v => !!v || 'タイトルは必須項目です。',
-          v => (v && v.length <= 10) || 'タイトルは10文字以内です。'
-        ],
-        author: '',
-        authorRules: [
-          v => !!v || '投稿者は必須項目です。',
-          v => (v && v.length <= 10) || '投稿者は10文字以内です。'
-        ],
-        post_date: '',
-        post_dateRules: [
-          v => !!v || '日付は必須項目です。',
-          v => (v && v.length <= 10) || '日付は10文字以内です。'
-        ],
-        content: '',
-        contentRules: [
-          v => !!v || '本文は必須項目です。',
-          v => (v && v.length <= 10) || '本文は10文字以内です。'
-        ],
-      methods: {
-        submit() {
-          // ↓***** 新規追加 & 修正
-          if (this.$refs.test_form.validate()) {
-          // すべてのバリデーションが通過したときのみ
-          // if文の中に入る
-          this.success = true;
-        } else {
-          this.success = false;
-        }         }  
-      },
-      components: {
-        AppBackgroundHolder,
-      },
+      title_f:'Form',
+      title: '',
+      author: '',
+      post_date: '',
+      content: '',
+      titleRules: [
+        v => !!v || 'タイトルは必須項目です。',
+        v => (v && v.length <= 10) || 'タイトルは10文字以内です。'
+      ],
+      authorRules: [
+        v => !!v || '投稿者は必須項目です。',
+        v => (v && v.length <= 10) || '投稿者は10文字以内です。'
+      ],
+      post_dateRules: [
+        v => !!v || '日付は必須項目です。',
+        v => (v && v.length <= 10) || '日付は10文字以内です。'
+      ],
+      contentRules: [
+        v => !!v || '本文は必須項目です。',
+        v => (v && v.length <= 10) || '本文は10文字以内です。'
+      ],
     }
-  }
+  },
+  methods: {
+    async submit() {
+      if (this.$refs.formref.validate()) {
+        // すべてのバリデーションが通過したときのみ
+        // if文の中に入る
+        this.success = true;
+        const params = {
+          title: this.title,
+          author: this.author,
+          post_date: this.post_date,
+          content: this.content,
+            }
+        const response = await axios.post('https://directus.minamirnd.work/items/articles', params)
+          response
+          .catch( error => {
+          console.log("response error", error)
+          return false 
+          })
+      } else {
+        this.success = false;
+      }        
+    }
+  } 
 }
 </script>
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '../router'
-import VCalendar from 'v-calendar';
+import VCalendar from 'v-calendar'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(VCalendar);
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(VCalendar);
+
 
 new Vue({
   router,
